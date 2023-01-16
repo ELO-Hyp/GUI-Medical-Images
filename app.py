50b8bccb8c8528912285cfbc6a49278ea4aaaf92
Lili <georgescu_lily.com>
2023-01-16 18:04:02 +0200

    add sr CT

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ import tkinter
 if __name__ == "__main__":
     multiprocessing.freeze_support()
     MainWindow(tkinter.Tk(), "ELO-Hyp")
+    
+    
+# pyinstaller app.py --onefile  -F --hiddenimport=pydicom.encoders.gdcm --hiddenimport=pydicom.encoders.pylibjpeg 
--- sr_window.py
+++ sr_window.py
@@ -7,13 +7,18 @@ import onnxruntime
 import numpy as np
 import pickle
 from functools import partial
+import glob
+import os
+import pydicom
+import pdb
+import matplotlib.pyplot as plt
 
 
 class SuperResolutionWindow:
     def __init__(self, window, window_title):
 
         self.window = window
-        self.window.grab_set() # Make it the main window.
+        self.window.grab_set()  # Make it the main window.
 
         self.review_enroll_window = None
         self.settings_window_created = False
@@ -63,7 +68,6 @@ class SuperResolutionWindow:
         self.button_saving_folder = Button(window, text="Browse folder", command=fct_folder_save)
         self.button_saving_folder.place(relx=0.05, rely=0.47)
 
-
         self.counter = 0
         self.processing_thread = None
 
@@ -83,6 +87,12 @@ class SuperResolutionWindow:
             self.window.grab_release() # Release the main.
             self.window.destroy()
 
+        # load network in memory
+        self.model_ct_x2 = onnxruntime.InferenceSession(os.path.join("resources_sr", "CT_x2_single.onnx"))
+        self.model_ct_x4 = onnxruntime.InferenceSession(os.path.join("resources_sr", "CT_x4_single.onnx"))
+        self.model_mri_x2 = onnxruntime.InferenceSession("super_resolution.onnx")
+        self.model_mri_x4 = onnxruntime.InferenceSession("super_resolution.onnx")
+
         self.window.protocol("WM_DELETE_WINDOW", on_closing)
 
     def select_folder(self, storing_label):
@@ -99,45 +109,98 @@ class SuperResolutionWindow:
 
         self.window.after(1000, self.__update)
 
-    def __test_onnx(self):
-        ort_session = onnxruntime.InferenceSession("super_resolution.onnx")
+    def __run_network(self, model, img):
+        lr = np.expand_dims(img, axis=(0, 1))
 
-        # def to_numpy(tensor):
-        #
-        #     return tensor.cpu().numpy() # tensor.detach().cpu().numpy() if tensor.requires_grad else
-
-        with open('test_samples\HR_PD_500_0_x2.pt', 'rb') as _f:
-            pd_lr = np.expand_dims(pickle.load(_f), axis=(0, 1))
-
-        pd_lr = np.float32(pd_lr)
-
+        lr = np.float32(lr)
         # compute ONNX Runtime output prediction
-        ort_inputs = {ort_session.get_inputs()[0].name: pd_lr}
-        ort_outs = ort_session.run(None, ort_inputs)
+        ort_inputs = {model.get_inputs()[0].name: lr}
+        ort_outs = model.run(None, ort_inputs)
 
-        print(ort_outs[0][0, 0].shape)
+        return ort_outs[0][0, 0]
 
-    def __process(self,  ):
+    def __read_CT(self, dicom_path: str):
+        dicom = pydicom.dcmread(dicom_path)
+        intercept = int(dicom[0x28, 0x1052].value)
+        pixels = dicom.pixel_array
+        padding = -2000
+        padding_location = np.where(pixels == padding)
+        pixels[pixels == padding] = 0
+
+        pixels = pixels + intercept
+        pixels = pixels / 1000
+        return dicom, pixels, padding_location, intercept
+
+    def __inverse_CT_value(self, pixels, intercept, scale, padding_location):
+        pixels = pixels * 1000
+        pixels = pixels - intercept
+
+        return pixels
+
+    def run_sr(self, file_path: str, scale: int, img_type: str):
         try:
-            for i in range(30):
-                if self.stop_thread is True:
-                    print("Self stop_thread", self.stop_thread)
-                    break
-                # time.sleep(1)
-                self.__test_onnx()
+            if img_type == 'CT':
+                dicom, pixels, padding_location, intercept = self.__read_CT(file_path)
+
+                if scale == 2:
+                    sr_pixels = self.__run_network(self.model_ct_x2, pixels)
+                elif scale == 4:
+                    sr_pixels = self.__run_network(self.model_ct_x4, pixels)
+                else:
+                    raise ValueError(f"Scale {scale} not supported!")
+                sr_pixels = self.__inverse_CT_value(sr_pixels, intercept=intercept, scale=scale,
+                                                    padding_location=padding_location)
+                # plt.subplot(1, 2, 1)
+                # plt.imshow(pixels, cmap='gray')
+                # plt.subplot(1, 2, 2)
+                # plt.imshow(sr_pixels, cmap='gray')
+                # plt.show()
+                dicom.PixelData = np.array(sr_pixels, np.int16).tobytes()
+                dicom.Rows = int(dicom.Rows * int(scale))
+                dicom.Columns = int(dicom.Columns * int(scale))
+            elif img_type == 'MRI':
+                pass
+            else:
+                raise ValueError(f"Image type {img_type} not supported!")
+        except Exception as ex:
+            print(ex)
+            self.__shown_text = "An exception occurred!"
+
+        return dicom
+
+    def __process(self, input_dir: str, output_dir: str, scale: int, img_type: str):
+        try:
+            file_paths = glob.glob(os.path.join(input_dir, '*'))
+            self.__num_of_processing_images = len(file_paths)
+            for file_path in file_paths:
+                dicom = self.run_sr(file_path, scale=scale, img_type=img_type)
+                path_to_save = os.path.join(output_dir, os.path.split(file_path)[-1])
+                dicom.save_as(path_to_save)
                 self.counter += 1
 
+            self.__shown_text = f"Results saved at: {output_dir}"
             self.button_start_processing.config(state='normal')
             self.counter = 0
         except Exception as ex:
             print(ex)
             self.__shown_text = "An exception occurred!"
 
-
     def __start_processing(self):
+
+        input_dir = self.label_folder_imgs_path["text"]
+        if input_dir is None or input_dir == "":
+            messagebox.showerror("Error", "The processing folder must be selected!")
+            return
+
+        save_dir = self.label_saving_folder_path["text"]
+        if save_dir is None or save_dir == "":
+            messagebox.showerror("Error", "The saving folder must be selected!")
+            return
 
         self.button_start_processing.config(state='disabled')
 
         self.processing_thread = threading.Thread(target=self.__process,
-                                                  args=(), daemon=True)
+                                                  args=(input_dir, save_dir,
+                                                        int(self.resolution_scale.get()),
+                                                        self.img_type.get(), ), daemon=True)
         self.processing_thread.start()
